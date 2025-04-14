2c57533bbbee45fd508faf3512bfc6a5042e0a7c
박기현 <kihyun1998@penta.co.kr>
2025-04-14 21:23:53 +0900

    FIX: fix format

--- lib/flutter_alone_method_channel.dart
+++ lib/flutter_alone_method_channel.dart
@@ -11,7 +11,8 @@ class MethodChannelFlutterAlone extends FlutterAlonePlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }
