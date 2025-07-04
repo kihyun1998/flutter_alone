8898b8ab13e80b4a5207ed47fe9e8deafa91dafb
Ki Hyun Park <kihyun1998@penta.co.kr>
2025-07-04 23:18:06 +0900

    start

---
+++ example/macos/Pods/Target Support Files/flutter_alone/flutter_alone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_flutter_alone : NSObject
+@end
+@implementation PodsDummy_flutter_alone
+@end
---
+++ example/macos/Pods/Target Support Files/system_tray/system_tray-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_system_tray : NSObject
+@end
+@implementation PodsDummy_system_tray
+@end
