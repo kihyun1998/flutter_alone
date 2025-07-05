cecc13f0a652face7ad1fa9bff86192dc6d8fef4
Ki Hyun Park <kihyun1998@penta.co.kr>
2025-07-05 18:43:27 +0900

    Revert "start"
    This reverts commit 8898b8ab13e80b4a5207ed47fe9e8deafa91dafb.

--- example/macos/Pods/Target Support Files/flutter_alone/flutter_alone-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_alone : NSObject
-@end
-@implementation PodsDummy_flutter_alone
-@end
--- example/macos/Pods/Target Support Files/system_tray/system_tray-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_system_tray : NSObject
-@end
-@implementation PodsDummy_system_tray
-@end
