c1dd00f1190d080e060b3ed39af0d3576d8af803
Ki Hyun Park <kihyun1998@penta.co.kr>
2025-07-05 18:43:21 +0900

    Revert "update: 유틸 구현"
    This reverts commit ca25733ded62f028703317acddea45f0fb5a7f0b.

--- macos/Classes/ProcessUtils.swift
+++ macos/Classes/ProcessUtils.swift
@@ -2,16 +2,10 @@ import Foundation
 
 class ProcessUtils {
     /// Checks if a process with the given process ID (PID) is currently running.
-    /// This is done by checking if a process group ID can be retrieved for the PID.
-    /// If the process does not exist, `getpgid` will fail.
     /// - Parameter pid: The process ID to check.
     /// - Returns: `true` if the process is running, `false` otherwise.
     static func isProcessRunning(pid: pid_t) -> Bool {
-        // A pid of 0 is not a valid process ID.
-        if pid <= 0 {
-            return false
-        }
-        // getpgid returns -1 if the process does not exist.
-        return getpgid(pid) != -1
+        // To be implemented in the next step.
+        return false
     }
 }
